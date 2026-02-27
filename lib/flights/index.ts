d0e6658d0523e1812462f68753344fc398257f5f
Ray Manaloto <rmanaloto@Rays-MacBook-Pro.local>
2026-02-26 16:08:19 -0800

    feat(flights): add barrel export for flights module
    Re-export all public APIs (getIataCode, hasIataCode,
    computeFlightLegs, searchFlights, formatDuration, resetTokenCache)
    and types (FlightLeg, GigForLeg, FlightOffer) from lib/flights.
    
    Closes part of #62

---
+++ lib/flights/index.ts
@@ -0,0 +1,5 @@
+export { getIataCode, hasIataCode } from "./iata-codes";
+export { computeFlightLegs } from "./compute-legs";
+export type { GigForLeg, FlightLeg } from "./compute-legs";
+export { searchFlights, formatDuration, resetTokenCache } from "./amadeus";
+export type { FlightOffer } from "./amadeus";
